dca2a3ea64aabee54cf3794dddb008889d821df9
Evgeny <angst_eam@mail.ru>
2021-02-14 20:06:54 +1000

    1. Создать jsp-страницы и сервлеты-контроллеры для сущностей Пользователь (User) и Категория продукта (Category). Можно для какой-то одной, но лучше создать обе.
    2. Реализовать для этих сущностей весь функционал, показанный преподавателем на уроке для сущности Product.
    
    3. Добавить к этому функционалу возможность создавать новый экземпляр сущности и удалять сущность.
    
    4. Можно и нужно использовать те же HTML шаблоны, что на уроке использовал преподаватель.

---
+++ first-web-app/src/main/java/ru/geekbrains/UserServlet.java
@@ -0,0 +1,39 @@
+package ru.geekbrains;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.geekbrains.persist.Product;
+import ru.geekbrains.persist.ProductRepository;
+import ru.geekbrains.persist.UserRepository;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.math.BigDecimal;
+
+@WebServlet(urlPatterns = "/user/*")
+public class UserServlet extends HttpServlet {
+
+    private UserRepository userRepository;
+
+    @Override
+    public void init() throws ServletException {
+        this.userRepository = (UserRepository) getServletContext().getAttribute("userRepository");
+        if (userRepository == null) {
+            throw new ServletException("UserRepository not initialized");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        if (req.getPathInfo() == null || req.getPathInfo().equals("/")) {
+            req.setAttribute("users", userRepository.findAll());
+            getServletContext().getRequestDispatcher("/WEB-INF/user.jsp").forward(req, resp);
+        }
+    }
+
+}
